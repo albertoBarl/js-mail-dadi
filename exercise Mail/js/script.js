2104d74e58dd7f1592bc821cf1e9024d62f23c5e
albertoBarl <alberto.barletta00@gmail.com>
2022-11-30 15:43:12 +0100

    started script

--- exercise Mail/js/script.js
+++ exercise Mail/js/script.js
@@ -3,3 +3,17 @@
 // 3- Stampa messaggio
 // 3.1 IF- Può accedere
 // 3.2 ELSE- Non può accedere
+
+const container =document.querySelector('col-12')
+
+const email_array =['aldo@mail.1', 'giovanni@mail.2', 'giacomo@mail.3'];
+let mail = prompt('Inserisci la tua email');
+
+    if(mail = email_array[undefined]){
+        mail = true
+        console.log('Accesso avvenuto')
+    }
+
+    else{
+        console.log('Accesso negato')
+    }
