8768bbe978e0a7127b537e1225278fff49661ceb
albertoBarl <alberto.barletta00@gmail.com>
2022-12-01 10:27:48 +0100

    fixed .js dadi

--- exercise Gioco dei dadi/js/script.js
+++ exercise Gioco dei dadi/js/script.js
@@ -7,12 +7,15 @@
 let player_num;
 let AI_num;
 
-player_num = (Math.random(1) * 6).toFixed(0);
-AI_num = (Math.random(1) * 6).toFixed(0);
+player_num = ((Math.random() * 6)+1).toFixed(0);
+AI_num = ((Math.random() * 6)+1).toFixed(0);
 
 if(player_num > AI_num){
     alert(`Con ${player_num} ha vinto il PLAYER`);
 
+}
+else if(player_num = AI_num){
+    alert(`${player_num} E ${AI_num}, AVETE PAREGGIATO!`)
 }
 else{
     alert(`Con ${AI_num} ha vinto l'AI`);
