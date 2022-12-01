1720e7a22fbb8af1255d0b3ebff21a9f2aef345d
albertoBarl <alberto.barletta00@gmail.com>
2022-12-01 09:56:30 +0100

    cleared javascript

--- exercise Mail/js/script.js
+++ exercise Mail/js/script.js
@@ -5,28 +5,26 @@
 // 3.2 ELSE- Non può accedere
 
 const container =document.querySelector('div.col-12')
+
 const email_array =['aldo@mail.1', 'giovanni@mail.2', 'giacomo@mail.3'];
 let mail = prompt('Inserisci la tua email');
 let m_try = false;
+let element;
 
 for(i=0; i<email_array.length; i++){
-    let element;
 
     if(mail == email_array[i]){
         m_try = true
     }
-    else{
-    }
-
-    if(m_try === true){
-        element = `<div class="bg-danger text-white">${mail} ACCESSO AVVENUTO</div>`
-        console.log('Accesso avvenuto');
-    }
-    else{
-        element = `<div class="bg-danger text-white">${mail} ACCESSO NEGATO</div>`
-        console.log('Accesso negato')
-    }
-    
-    container.innerHTML = element;
-
 }
+
+if(m_try === true){
+    element = `<div class="bg-danger text-white">${mail} ACCESSO AVVENUTO</div>`
+    console.log('Accesso avvenuto');
+}
+else{
+    element = `<div class="bg-danger text-white">${mail} ACCESSO NEGATO</div>`
+    console.log('Accesso negato')
+}
+
+container.innerHTML = element;
