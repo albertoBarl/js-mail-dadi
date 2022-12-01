1d0b1a84be3dadc9268aa97c2540698d34691314
albertoBarl <alberto.barletta00@gmail.com>
2022-12-01 09:41:40 +0100

    added message

--- exercise Mail/js/script.js
+++ exercise Mail/js/script.js
@@ -4,12 +4,13 @@
 // 3.1 IF- Può accedere
 // 3.2 ELSE- Non può accedere
 
-const container =document.querySelector('col-12')
+const container =document.querySelector('div.col-12')
 const email_array =['aldo@mail.1', 'giovanni@mail.2', 'giacomo@mail.3'];
 let mail = prompt('Inserisci la tua email');
 let m_try = false;
 
 for(i=0; i<email_array.length; i++){
+    let element;
 
     if(mail == email_array[i]){
         m_try = true
@@ -18,10 +19,14 @@ for(i=0; i<email_array.length; i++){
     }
 
     if(m_try === true){
+        element = `<div class="bg-danger text-white">${mail} ACCESSO AVVENUTO</div>`
         console.log('Accesso avvenuto');
     }
     else{
+        element = `<div class="bg-danger text-white">${mail} ACCESSO NEGATO</div>`
         console.log('Accesso negato')
     }
     
+    container.innerHTML = element;
+
 }
