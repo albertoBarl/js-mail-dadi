b52375e9e7ef3e5963f2d21ff5548d061c9911c7
albertoBarl <alberto.barletta00@gmail.com>
2022-12-01 10:09:18 +0100

    added content .js  gioco dei dadi

--- exercise Gioco dei dadi/js/script.js
+++ exercise Gioco dei dadi/js/script.js
@@ -3,3 +3,33 @@
 // 3- Stabilire vincitore
 // 3.1 IF player 1 > AI, vincitore
 // 3.2 ELSE perdente
+
+let player_num;
+let AI_num;
+
+player_num = (Math.random(1) * 6).toFixed(0);
+AI_num = (Math.random(1) * 6).toFixed(0);
+
+if(player_num > AI_num){
+    alert(`Con ${player_num} ha vinto il PLAYER`);
+
+}
+else{
+    alert(`Con ${AI_num} ha vinto l'AI`);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
